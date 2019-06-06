07d0a271e5e2527061038e9d695da6ec663f9443
LEFEBVRE Nicolas <NLV@dsone.3ds.com>
2019-06-06 17:11:40 -0400

    Add color shifting wheels, narrower speedo

--- MW2_final/MW2_final.ino
+++ MW2_final/MW2_final.ino
@@ -20,7 +20,9 @@ CRGB speedo[NUM_LEDS_SPEEDO];
 int maxAnalogRead = 600;
 int minAnalogRead = 0;
 int currentHeadlightColor = 0;
+unsigned short currentHue = 128;
 
+bool colorShifting = false;
 bool globalOn = false;
 
 void setAll(CRGB c, CRGB* strip, unsigned int numLeds) {
@@ -45,6 +47,7 @@ void handleButtons(AceButton* button, uint8_t eventType, uint8_t buttonState) {
           break;
         case COLOR_BUTTON_ID:
           setAll(headlightColors[++currentHeadlightColor % HEADLIGHT_COLORS_COUNT], headlight, NUM_LEDS_HEADLIGHT);
+          colorShifting = true;
           Serial.println("color");
           break;
         case HORN_BUTTON_ID:
@@ -58,7 +61,9 @@ void handleButtons(AceButton* button, uint8_t eventType, uint8_t buttonState) {
           globalOn = false;
           allLEDsOff();
           break;
-        case COLOR_BUTTON_ID: // fall through on purpose
+        case COLOR_BUTTON_ID:
+          colorShifting = false;
+          break;
         case HORN_BUTTON_ID:
           // nothing to do on release for either of these
           break;
@@ -70,7 +75,7 @@ void handleButtons(AceButton* button, uint8_t eventType, uint8_t buttonState) {
 void allLEDsOff() {
   setAll(CRGB::Black, frontWheel, NUM_LEDS_FRONT_WHEEL);
   setAll(CRGB::Black, backWheel, NUM_LEDS_BACK_WHEEL);
-  //setAll(CRGB::Black, sideLights, NUM_LEDS_SIDE_LIGHTS);
+  setAll(CRGB::Black, sideLights, NUM_LEDS_SIDE_LIGHTS);
   setAll(CRGB::Black, headlight, NUM_LEDS_HEADLIGHT);
   setAll(CRGB::Black, speedo, NUM_LEDS_SPEEDO);
   showStrips();
@@ -90,7 +95,7 @@ void updateSpeedo(unsigned short val) {
   for (unsigned int i = 0; i < NUM_LEDS_SPEEDO; ++i) {
     if (i < speedoVal)
       //speedo[i] = CHSV(i*4, 255, 128);
-      speedo[i] = CRGB(constrain(i*12, 0, 255), constrain(255-i*12, 0, 255), 0);
+      speedo[i] = CRGB(constrain(i*12, 0, 255), constrain(255-i*12, 0, 255), 0) % (255*0.3);
     else
       speedo[i] = CRGB::Black;
   }
@@ -101,7 +106,7 @@ void outputThrottlePositionToSerial(unsigned short val) {
   unsigned long currentTime = millis();
   float elapsed = currentTime - previousUpdate;
 
-  if (elapsed < 16) {
+  if (elapsed < 50) {
     return; 
   }
   previousUpdate = currentTime;
@@ -122,71 +127,98 @@ bool isWithinChase(unsigned int pos, unsigned int center, unsigned int NUM_LEDS,
   return distanceFromCenterOfChase(pos, center, NUM_LEDS) <= CHASE_LENGTH/2;
 }
 
-void animateFrontWheel(unsigned short val) {
+void animateFrontWheel(unsigned short rpm, CRGB* strip, unsigned short numLeds, unsigned short offset, unsigned short chaseLength) {
   static unsigned long previousUpdate = millis();
   unsigned long currentTime = millis();
   float elapsed = currentTime - previousUpdate;
 
-  unsigned short rpm = constrain(map(val, minAnalogRead, maxAnalogRead-5, MIN_RPM, MAX_RPM), MIN_RPM, MAX_RPM);
-
   float rotateBy = (elapsed/60000)*rpm;
-  static short chaseCenter = FRONT_OFFSET;
-  short newChaseCenter = chaseCenter + NUM_LEDS_FRONT_WHEEL*rotateBy;
+  static unsigned short chaseCenter = offset;
+  unsigned short newChaseCenter = chaseCenter + numLeds*rotateBy;
 
   if (newChaseCenter == chaseCenter)
     return;
 
   chaseCenter = newChaseCenter;
   previousUpdate = currentTime;
-  if (chaseCenter >= NUM_LEDS_FRONT_WHEEL)
+  if (chaseCenter >= numLeds)
     chaseCenter = 0;
 
-  for (unsigned int i = 0; i < NUM_LEDS_FRONT_WHEEL; ++i) {
-    unsigned int real_i = i%NUM_LEDS_FRONT_WHEEL;
-    if (isWithinChase(real_i, chaseCenter, NUM_LEDS_FRONT_WHEEL, CHASE_LENGTH_FRONT)) { // add energy based on distance from center of chase
-      float distance = distanceFromCenterOfChase(real_i, chaseCenter, NUM_LEDS_FRONT_WHEEL);
-      short energy = MAX_ENERGY - min(MAX_ENERGY, distance * 10);
-      frontWheel[real_i] += CRGB(0, energy, energy);
+  for (unsigned short i = 0; i < numLeds; ++i) {
+    unsigned short real_i = i%numLeds;
+    if (isWithinChase(real_i, chaseCenter, numLeds, chaseLength)) { // add energy based on distance from center of chase
+      float distance = distanceFromCenterOfChase(real_i, chaseCenter, numLeds);
+      unsigned short energy = MAX_ENERGY - min(MAX_ENERGY, distance * 10);
+      //strip[real_i] += CRGB(0, energy, energy);
+      strip[real_i] += CHSV(currentHue, 255, energy);
     } else { // decay
-      frontWheel[real_i].nscale8(DECAY_RATE);
+      strip[real_i].nscale8(DECAY_RATE);
     }
   }
-  frontWheel[FRONT_OFFSET] = CRGB::Red;
 }
 
-// should this be refactored to not be a disgusting copy-paste of the front wheel? Yes. Do I have the energy at 11PM 2 days before reveal? Nope.
-void animateBackWheel(unsigned short val) {
+// Yes, this is 100% identical to animateFrontWheel, but it looks like trying to share the same function prevents some compiler optimizations and makes everything slower...
+void animateBackWheel(unsigned short rpm, CRGB* strip, unsigned short numLeds, unsigned short offset, unsigned short chaseLength) {
   static unsigned long previousUpdate = millis();
   unsigned long currentTime = millis();
   float elapsed = currentTime - previousUpdate;
 
-  unsigned short rpm = constrain(map(val, minAnalogRead, maxAnalogRead-5, MIN_RPM, MAX_RPM), MIN_RPM, MAX_RPM);
-
   float rotateBy = (elapsed/60000)*rpm;
-  static short chaseCenter = BACK_OFFSET;
-  short newChaseCenter = chaseCenter + NUM_LEDS_BACK_WHEEL*rotateBy;
+  static unsigned short chaseCenter = offset;
+  unsigned short newChaseCenter = chaseCenter + numLeds*rotateBy;
 
   if (newChaseCenter == chaseCenter)
     return;
 
   chaseCenter = newChaseCenter;
   previousUpdate = currentTime;
-  if (chaseCenter >= NUM_LEDS_BACK_WHEEL)
+  if (chaseCenter >= numLeds)
     chaseCenter = 0;
 
-  for (unsigned int i = 0; i < NUM_LEDS_BACK_WHEEL; ++i) {
-    unsigned int real_i = i%NUM_LEDS_BACK_WHEEL;
-    if (isWithinChase(real_i, chaseCenter, NUM_LEDS_BACK_WHEEL, CHASE_LENGTH_BACK)) { // add energy based on distance from center of chase
-      float distance = distanceFromCenterOfChase(real_i, chaseCenter, NUM_LEDS_BACK_WHEEL);
-      short energy = MAX_ENERGY - min(MAX_ENERGY, distance * 10);
-      backWheel[real_i] += CRGB(0, energy, energy);
+  for (unsigned short i = 0; i < numLeds; ++i) {
+    unsigned short real_i = i%numLeds;
+    if (isWithinChase(real_i, chaseCenter, numLeds, chaseLength)) { // add energy based on distance from center of chase
+      float distance = distanceFromCenterOfChase(real_i, chaseCenter, numLeds);
+      unsigned short energy = MAX_ENERGY - min(MAX_ENERGY, distance * 10);
+      //strip[real_i] += CRGB(0, energy, energy);
+      strip[real_i] += CHSV(currentHue, 255, energy);
     } else { // decay
-      backWheel[real_i].nscale8(DECAY_RATE);
+      strip[real_i].nscale8(DECAY_RATE);
     }
   }
-  backWheel[BACK_OFFSET] = CRGB::Red;
 }
 
+void animateSideLights(unsigned short rpm, CRGB* strip, unsigned short numLeds) {
+  static unsigned long previousUpdate = millis();
+  unsigned long currentTime = millis();
+  float elapsed = currentTime - previousUpdate;
+
+  if (elapsed < 50) {
+    return;
+  }
+  previousUpdate = currentTime;
+  
+  unsigned short energy = beatsin8(rpm, 0, MAX_ENERGY);
+  CRGB color = CRGB(0, energy, energy) % (255*0.3);
+  for (unsigned int i = 0; i < numLeds; ++i) {
+    strip[i] = CRGB(0, energy, energy);
+  }
+}
+
+void shiftColors() {
+  if (!colorShifting)
+    return;
+  static unsigned long previousUpdate = millis();
+  unsigned long currentTime = millis();
+  float elapsed = currentTime - previousUpdate;
+
+  if (elapsed < 50) {
+    return; 
+  }
+  previousUpdate = currentTime;
+    
+  ++currentHue;
+}
 
 void setup() {
   pinMode(SW_0_PIN, INPUT_PULLUP);
@@ -205,7 +237,7 @@ void setup() {
   FastLED.addLeds<SK9822, STRIP_2_DATA, STRIP_2_CLOCK, BGR>(backWheel, NUM_LEDS_BACK_WHEEL);
   // NB: attach other side of rear to power and GND for STRIP_3, then attach data and clock to STRIP_2
   // ==== STRIP_4 NOT CONNECTED ====
-  //FastLED.addLeds<SK9822, STRIP_5_DATA, STRIP_5_CLOCK, BGR>(sideLights, NUM_LEDS_SIDE_LIGHTS);
+  FastLED.addLeds<SK9822, STRIP_5_DATA, STRIP_5_CLOCK, BGR>(sideLights, NUM_LEDS_SIDE_LIGHTS);
   // NB: attach other side of sidelights to power and GND for STRIP_6, then attach data and clock to STRIP_5
   // ==== STRIP_7 NOT CONNECTED ====
   FastLED.addLeds<WS2812B, STRIP_8_DATA, GRB>(speedo, NUM_LEDS_SPEEDO);
@@ -238,8 +270,10 @@ void loop() {
 
   outputThrottlePositionToSerial(analogVal);
   updateSpeedo(analogVal);
-  animateFrontWheel(analogVal);
-  animateBackWheel(analogVal);
-    
+  unsigned short rpm = constrain(map(analogVal, minAnalogRead, maxAnalogRead-5, MIN_RPM, MAX_RPM), MIN_RPM, MAX_RPM);
+  animateFrontWheel(rpm, frontWheel, NUM_LEDS_FRONT_WHEEL, FRONT_OFFSET, CHASE_LENGTH_FRONT);
+  animateBackWheel(rpm, backWheel, NUM_LEDS_BACK_WHEEL, BACK_OFFSET, CHASE_LENGTH_BACK);
+  animateSideLights(rpm, sideLights, NUM_LEDS_SIDE_LIGHTS);
+  shiftColors();
   showStrips();
 }
--- MW2_final/config.h
+++ MW2_final/config.h
@@ -4,17 +4,17 @@
 
 #define NUM_LEDS_FRONT_WHEEL 96
 #define NUM_LEDS_BACK_WHEEL 120
-#define NUM_LEDS_SIDE_LIGHTS 200 // FIXME
+#define NUM_LEDS_SIDE_LIGHTS 200 // FIXME 73 + 
 #define NUM_LEDS_HEADLIGHT 40
-#define NUM_LEDS_SPEEDO 22 // NB: there are technically 24 of these but we want a "dead zone"  at the bottom
+#define NUM_LEDS_SPEEDO 19 // NB: there are technically 24 of these but we want a "dead zone"  at the bottom
 
 #define FRONT_OFFSET 22
 #define BACK_OFFSET 82
 
 #define MIN_RPM 15
-#define MAX_RPM 300
+#define MAX_RPM 150
 
-#define MAX_ENERGY 255
+#define MAX_ENERGY 100
 #define DECAY_RATE 0.94*256 // on every step, an LED outside of the chase retains 94% energy from previous step
 #define CHASE_LENGTH_FRONT 7
 #define CHASE_LENGTH_BACK 7
